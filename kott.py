582c19eb89ea3295ab12b4d0beffe7351e82f4fa
Tacolizard <tacolizardfilms@gmail.com>
2016-04-18 08:03:15 -0700

    Added files via upload

--- kott.py
+++ kott.py
@@ -7,10 +7,32 @@ ui = WebUI(app, debug=False) # Create a WebUI instance
 
 #open html files
 indexfile = file.read(open('index.html'))
+stylesht = file.read(open('static/stylesheets/styles.css'))
+mejs = file.read(open('js/medium-editor.js'))
+mecss = file.read(open('css/medium-editor.css'))
+methm = file.read(open('css/themes/default.css'))
+#/js/medium-editor.js
+
 
 @app.route("/")
 def index():
     return indexfile
+
+@app.route("/static/stylesheets/styles.css")
+def styles():
+    return stylesht
+
+@app.route("/js/medium-editor.js")
+def mediumjs():
+    return mejs
+
+@app.route("/css/medium-editor.css")
+def mediumcss():
+    return mecss
+
+@app.route("/css/themes/default.css")
+def mediumtheme():
+    return methm
 
 
 if __name__ == '__main__':
